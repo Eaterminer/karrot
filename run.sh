b6f93910d1d22840920c8639da1db025ef6abeb6
EaterMiner <ptooffical@gmail.com>
2023-02-12 12:31:07 +0000

    Updated tunnel url Automatically.

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-cd output
-curl -s 10.42.0.1:4040/api/tunnels | jq -r .tunnels[0].public_url > url.txt
+curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url > url.txt
 git add .
 git commit -m "Updated tunnel url Automatically."
 git push
-cd ./
